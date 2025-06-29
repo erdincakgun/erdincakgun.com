faebaf95e1ece78cbcc959a16a92a541a7c88fae
Erdinç Akgün <erdinc.akgun@icloud.com>
2025-06-29 12:36:49 +0200

    Refactor Navbar and App components for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,19 @@ function App() {
         content="Hi, I'm Erdinç Akgün, a devops consultant from Türkiye. I've worked on projects ranging from real-time unmanned aerial vehicle systems to machine learning deployments and full-stack web applications."
       />
       <Navbar></Navbar>
-      <article class="prose p-2">
-        <p>Hi, I'm Erdinç Akgün, a devops consultant from Türkiye. I've worked on projects ranging from real-time unmanned aerial vehicle systems to machine learning deployments and full-stack web applications.</p>
-        <h2>Contact</h2>
-        <a href="mailto:hello@erdincakgun.com?subject=Website%20Contact%20-%20erdincakgun.com&body=Hi%20Erdinç,%20">hello@erdincakgun.com</a>
-      </article>
+      <div className="mx-auto max-w-4xl">
+        <article class="prose p-2 max-w-none w-full">
+          <p>
+            Hi, I'm Erdinç Akgün, a devops consultant from Türkiye. I've worked
+            on projects ranging from real-time unmanned aerial vehicle systems
+            to machine learning deployments and full-stack web applications.
+          </p>
+          <h2>Contact</h2>
+          <a href="mailto:hello@erdincakgun.com?subject=Website%20Contact%20-%20erdincakgun.com&body=Hi%20Erdinç,%20">
+            hello@erdincakgun.com
+          </a>
+        </article>
+      </div>
     </>
   );
 }
